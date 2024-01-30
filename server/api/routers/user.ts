0e7a9a3992874f52d5567c9702617d3f06884236
acatzk <joshuaimalay@gmail.com>
2024-01-30 14:20:18 +0800

    feat(be): implement suggested users api

--- server/api/routers/user.ts
+++ server/api/routers/user.ts
@@ -23,5 +23,42 @@ export const userRouter = createTRPCRouter({
           username: input.username
         }
       })
+    }),
+
+  getSuggestedUsers: protectedProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.number().nullish() // <-- "cursor" needs to exist, but can be any type
+      })
+    )
+    .query(async (opts) => {
+      const { ctx, input } = opts
+      const limit = input.limit ?? 15
+      const { cursor } = input
+
+      const users = await ctx.db.user.findMany({
+        take: limit + 1, // get an extra item at the end which we'll use as next cursor
+        cursor: cursor ? { id: cursor } : undefined,
+        where: {
+          externalId: {
+            not: ctx.auth.userId
+          }
+        },
+        orderBy: {
+          createdAt: 'desc'
+        }
+      })
+
+      let nextCursor: typeof cursor | undefined = undefined
+      if (users.length > limit) {
+        const nextItem = users.pop()
+        nextCursor = nextItem!.id
+      }
+
+      return {
+        users,
+        nextCursor
+      }
     })
 })
